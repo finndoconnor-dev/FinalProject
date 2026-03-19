eac2b9ade3196c83b4bca9f3d849348df06f00be
ril4456 <dylan.j.nunes@gmail.com>
2026-03-19 12:56:29 -0400

    Map switch
    I got a very simple map switching for two subLevels using an area2D node and adding each map as a child of baseLayer

---
+++ final-project-group-6-comp-1501/scenes/demo/base_layers.gd
@@ -0,0 +1,25 @@
+extends Node2D
+@export var map_scene: PackedScene
+@onready var mapPaths: Array[String]=["res://level_1S1.tscn","res://level_1s_2.tscn"]
+@export var mapCount=0
+var map: Node
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	map_scene=load(mapPaths[mapCount])
+	map=map_scene.instantiate()
+	map.mapExited.connect(changeMap)
+	self.add_child(map)
+
+func changeMap():
+	self.remove_child(map)
+	map.disconnect("mapExited",changeMap)
+	mapCount+=1
+	map_scene=load(mapPaths[mapCount])
+	map=map_scene.instantiate()
+	self.add_child(map)
+	print("Should be map2")
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
