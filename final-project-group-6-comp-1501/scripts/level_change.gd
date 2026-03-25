42e483c71b864d8418611f5d93c0ebb6631a570e
ril4456 <dylan.j.nunes@gmail.com>
2026-03-25 15:43:53 -0400

    made a boss level (open to changes))

--- final-project-group-6-comp-1501/scripts/level_change.gd
+++ final-project-group-6-comp-1501/scripts/level_change.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export var outgoingLevel : PackedScene
 
+	
 func _on_body_entered(body) -> void:
 	print("Level Exit Entered.")
 	if body.is_in_group("player"):
@@ -14,3 +15,5 @@ func _on_body_exited(body) -> void:
 
 func switchScene(player : PackedScene):
 	get_tree().change_scene_to_packed(outgoingLevel)
+	
+	
