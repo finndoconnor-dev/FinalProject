81f5ede520d2976fe6abc5fb1b30d3e94eedb0cf
ril4456 <dylan.j.nunes@gmail.com>
2026-04-03 19:03:15 -0400

    Reapply "stuff"
    This reverts commit 76090d29c017365ecc3b243911c686a3d213bd2c.

--- final-project-group-6-comp-1501/game_controller.gd
+++ final-project-group-6-comp-1501/game_controller.gd
@@ -9,11 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("pause"):
-		print("Pause working?")
 		pauseGame()
 
 func pauseGame():
-	print("Paused???")
 	get_tree().paused=true
 	$PauseMenu.visible=true
 	$DemoLevel/YsortingContainer/Player/PlayerHUD.hide()
