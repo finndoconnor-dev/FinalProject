378b0f1c2e935e011151f7d14e0d56bbf6c72017
Danny B <61749098+GameBox420@users.noreply.github.com>
2026-04-04 23:08:44 -0400

    export

--- final-project-group-6-comp-1501/scripts/global scripts/LevelTransition.gd
+++ final-project-group-6-comp-1501/scripts/global scripts/LevelTransition.gd
@@ -4,12 +4,16 @@ var playerInventory : Array[gun]
 
 
 func saveGunToCache(gun):
+	if playerInventory.has(gun):
+		return
 	playerInventory.append(gun)
 	add_child(gun)
 
 func loadGunsFromCache() -> Array:
 	var data = []
 	for i in playerInventory:
-		remove_child(i)
+		if i.get_parent() == self:
+			remove_child(i)
 		data.append(i)
+	playerInventory.clear()
 	return data
