a11d2335f4c5a9b2713a0b73ad9fad89a68263c7
Danny B <61749098+GameBox420@users.noreply.github.com>
2026-04-05 22:48:33 -0400

    the zawg

--- final-project-group-6-comp-1501/dog.gd
+++ final-project-group-6-comp-1501/dog.gd
@@ -1,12 +1,23 @@
 extends Node2D
 
-@export var speed=150
+@export var runSpeed=150
+
+var hasPlayed = false
+var speed = 0
+
+func _ready() -> void:
+	bark()
+	await get_tree().create_timer(1.5).timeout
+	speed = runSpeed
 
 func _process(delta: float) -> void:
 	position.y -= speed*delta
 	$AnimatedSprite2D.play("runAway")
-	await get_tree().create_timer(1.0).timeout
+	#if !hasPlayed:
+		#bark()
+	if (position.y < -1500):
+		queue_free()
+	
+func bark() -> void:
 	$AudioStreamPlayer.play()
-	await get_tree().create_timer(2.0).timeout
-	$AudioStreamPlayer.stop()
 	
